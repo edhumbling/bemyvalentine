9fda596c42712ae091aa92e3899d49341f833d75
Emmanuel Dwamena <emmergraix@gmail.com>
2025-02-12 17:29:54 +0000

    Fixed PrivacyDisclaimer component logic

---
+++ src/components/PrivacyDisclaimer.js
@@ -0,0 +1,9 @@
+function PrivacyDisclaimer() {
+  return (
+    <div className="privacy-disclaimer">
+      {/* Single instance of your content */}
+    </div>
+  );
+}
+
+export default PrivacyDisclaimer; 
