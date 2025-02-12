832fe1e7f40a7af6fef9d047024d4345fcfc516f
Emmanuel Dwamena <emmergraix@gmail.com>
2025-02-12 17:33:59 +0000

    Removed all privacy disclaimer instances

--- src/components/PrivacyDisclaimer.js
+++
@@ -1,9 +0,0 @@
-function PrivacyDisclaimer() {
-  return (
-    <div className="privacy-disclaimer">
-      {/* Single instance of your content */}
-    </div>
-  );
-}
-
-export default PrivacyDisclaimer; 
