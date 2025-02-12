2ad9803cecbd3870d281b213ad41389a4ab0f633
Emmanuel Dwamena <emmergraix@gmail.com>
2025-02-12 09:04:05 +0000

    Added footer to website

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="text-center py-4 text-sm text-gray-500">
+      <p>All Rights Reserved. Developed by LeadTech LTD - 0208705290</p>
+    </footer>
+  );
+};
+
+export default Footer; 
