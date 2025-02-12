f482cbc7ac04d7dfd7087761dfe28a97ff2dcdb7
Emmanuel Dwamena <emmergraix@gmail.com>
2025-02-12 09:57:48 +0000

    Improved video modal close button and loading

--- src/components/VideoModal.tsx
+++ src/components/VideoModal.tsx
@@ -11,19 +11,20 @@ const VideoModal = () => {
       <div className="relative w-full h-full md:h-auto md:max-w-4xl">
         <button
           onClick={() => setIsOpen(false)}
-          className="absolute -top-10 right-0 p-2 text-white hover:text-gray-300 transition-colors z-50 md:block"
+          className="absolute top-4 right-4 md:-top-10 md:right-0 p-3 bg-black/50 hover:bg-black/70 rounded-full text-white hover:text-gray-300 transition-colors z-50"
           aria-label="Close video"
         >
-          <X className="w-6 h-6" />
+          <X className="w-8 h-8 md:w-6 md:h-6" />
         </button>
         
         <div className="relative w-full h-full md:pt-[56.25%]">
           <iframe
-            src="https://fast.wistia.net/embed/iframe/gkkws8y4s5?autoPlay=true"
+            src="https://fast.wistia.net/embed/iframe/gkkws8y4s5?autoPlay=true&preload=true"
             className="absolute top-0 left-0 w-full h-full md:rounded-lg"
             title="Valentine Video"
             allow="autoplay; fullscreen"
             allowFullScreen
+            loading="eager"
           ></iframe>
         </div>
       </div>
